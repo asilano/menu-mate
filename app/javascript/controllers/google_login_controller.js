29a75996dc735034f6873b012fd93fe2ecf2eac2
Chris Howlett <chris.howlett@freeagent.com>
2024-02-14 23:21:51 +0000

    Enable Google Sign In; dead code warning!

--- app/javascript/controllers/google_login_controller.js
+++ app/javascript/controllers/google_login_controller.js
@@ -1,5 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
+function callback() {
+  console.log("Callback!")
+}
+
 export default class extends Controller {
   static values = { clientId: String };
   static targets = [ "signIn" ]
@@ -8,15 +12,12 @@ export default class extends Controller {
     const google = globalThis.google
     google.accounts.id.initialize({
       client_id: this.clientIdValue,
-      callback: this.callback,
+      callback: callback,
       cancel_on_tap_outside: false,
     });
     google.accounts.id.renderButton(this.signInTarget, {
-
+      theme: "outline",
+      size: "large",
     }, [])
-  }
-
-  callback() {
-    console.log("Callback!")
   }
 }
