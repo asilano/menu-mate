444ad9ec4f296324e7e52a5da67cca6f3f55945a
Chris Howlett <chris.howlett@freeagent.com>
2024-02-14 22:14:11 +0000

    Start plumbing in Google Sign-In

--- app/javascript/controllers/google_login_controller.js
+++ app/javascript/controllers/google_login_controller.js
@@ -1,8 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("Hello")
-    this.element.textContent = "Hello World!"
+  static values = { clientId: String };
+  static targets = [ "signIn" ]
+
+  setup() {
+    const google = globalThis.google
+    google.accounts.id.initialize({
+      client_id: this.clientIdValue,
+      callback: this.callback,
+      cancel_on_tap_outside: false,
+    });
+    google.accounts.id.renderButton(this.signInTarget, {
+
+    }, [])
+  }
+
+  callback() {
+    console.log("Callback!")
   }
 }
