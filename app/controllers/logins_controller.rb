67d8a1ad99a54489afc3930c970409c114bedeb7
Chris Howlett <chris.howlett@freeagent.com>
2024-02-15 16:09:33 +0000

    Handle response from Google to show success. Tidy up a bit

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,7 +1,22 @@
 class LoginsController < ApplicationController
+  before_action :validate_google_csrf, only: :create
+
   def new
   end
 
   def create
+    @google_account_info = Google::Auth::IDTokens.verify_oidc(
+      params[:credential],
+      aud: Rails.application.credentials.google_sign_in.client_id
+    )
+  end
+
+  private
+
+  def validate_google_csrf
+    # Google Sign In provides its own csrf token and stores it in the cookie along with including it in the request
+    if cookies["g_csrf_token"].blank? || params["g_csrf_token"].blank? || cookies["g_csrf_token"] != params["g_csrf_token"]
+      raise ActionController::InvalidAuthenticityToken
+    end
   end
 end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,3 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-
-import GoogleLoginController from "./google_login_controller"
-application.register("google-login", GoogleLoginController)
