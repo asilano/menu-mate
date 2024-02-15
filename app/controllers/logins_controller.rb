18b796b8ff56245098459e72ab854250f8613151
Chris Howlett <chris.howlett@freeagent.com>
2024-02-15 23:39:29 +0000

    Create or Update User from Google response

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,14 +1,30 @@
 class LoginsController < ApplicationController
-  before_action :validate_google_csrf, only: :create
+  before_action :validate_google_csrf, only: :google
 
   def new
   end
 
-  def create
+  def google
     @google_account_info = Google::Auth::IDTokens.verify_oidc(
       params[:credential],
       aud: Rails.application.credentials.google_sign_in.client_id
     )
+    google_userid = @google_account_info["sub"]
+
+    user = User.find_by(google_userid:)
+    if user
+      flash.notice = "Found existing user id #{user.id}"
+      user.update_from @google_account_info
+    else
+      user = User.create_from @google_account_info
+      if user
+        flash.notice = "Created new user id #{user.id}"
+      else
+        flash.alert = "Failed to create user"
+      end
+    end
+
+    redirect_to :root
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  validates :google_userid, presence: true
+
+  def self.create_from(google_account_info)
+    create(attributes_from google_account_info)
+  end
+
+  def update_from(google_account_info)
+    update(User.attributes_from google_account_info)
+  end
+
+  def self.attributes_from(google_account_info)
+    {
+      google_userid: google_account_info["sub"],
+      email: google_account_info["email"],
+      name: google_account_info["name"],
+      first_name: google_account_info["first_name"],
+      picture_uri: google_account_info["picture"]
+    }
+  end
 end
