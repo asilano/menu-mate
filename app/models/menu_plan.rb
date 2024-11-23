6e5df248cf87d631a3f3673ab0b3b0e1cb2b8766
Chris Howlett <chris.howlett@freeagent.com>
2024-11-23 20:21:06 +0000

    Force use of leftover sink following a leftover source

--- app/models/menu_plan.rb
+++ app/models/menu_plan.rb
@@ -4,9 +4,21 @@ class MenuPlan < ApplicationRecord
 
   def fill
     plan_days.each.with_object({ used_ids: [], leftovers: {} }) do |plan_day, state|
-      candidates = plan_day.candidate_recipes
-      meal = candidates.reject { |r| state[:used_ids].include?(r.id) }.sample
+      candidates = plan_day.candidate_recipes.reject { |r| state[:used_ids].include?(r.id) }
+
+      leftover_filtered_candidates = candidates.select do |candidate|
+        candidate.leftovers_sink&.leftover_id &&
+          state[:leftovers][candidate.leftovers_sink.leftover_id] &&
+          state[:leftovers][candidate.leftovers_sink.leftover_id] > 0
+      end
+
+      if !leftover_filtered_candidates.empty?
+        candidates = leftover_filtered_candidates
+      end
+
+      meal = candidates.sample
       plan_day.update(recipe: meal)
+
       state[:used_ids] << meal.id if meal
       if meal && meal.leftovers_source
         state[:leftovers][meal.leftovers_source.leftover.id] ||= 0
