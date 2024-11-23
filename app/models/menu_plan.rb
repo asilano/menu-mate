5f61ea5eee0ee5d7f5ede1958ae8933ba4f231bb
Chris Howlett <chris.howlett@freeagent.com>
2024-11-23 16:19:38 +0000

    Record use of leftovers source in the with_object of MenuPlan#fill

--- app/models/menu_plan.rb
+++ app/models/menu_plan.rb
@@ -3,11 +3,15 @@ class MenuPlan < ApplicationRecord
   has_many :plan_days, -> { order("day_number") }, dependent: :destroy
 
   def fill
-    plan_days.each.with_object([]) do |plan_day, used_ids|
+    plan_days.each.with_object({ used_ids: [], leftovers: {} }) do |plan_day, state|
       candidates = plan_day.candidate_recipes
-      meal = candidates.reject { |r| used_ids.include?(r.id) }.sample
+      meal = candidates.reject { |r| state[:used_ids].include?(r.id) }.sample
       plan_day.update(recipe: meal)
-      used_ids << meal.id if meal
+      state[:used_ids] << meal.id if meal
+      if meal && meal.leftovers_source
+        state[:leftovers][meal.leftovers_source.leftover.id] ||= 0
+        state[:leftovers][meal.leftovers_source.leftover.id] += meal.leftovers_source.num_days
+      end
     end
   end
 end
